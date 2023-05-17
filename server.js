00a6181ddd7f2f340ee9b172a48541a53f286eea
XyrillaSC <professionalstevenkramer@gmail.com>
2023-05-17 18:06:23 -0400

    added ids for new notes

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const fs = require('fs');
 const util = require('util');
+const { v4: uuidv4 } = require('uuid');
 
 const PORT = 3001;
 
@@ -17,39 +18,68 @@ const readFromFile = util.promisify(fs.readFile);
 
 // Alterative Write file
 const writeToFile = (destination, content) =>
-    fs.writeFile(destination, JSON.stringify(content, null, 4), (err) =>
-        err ? console.error(err) : console.info(`\nData written to ${destination}`)
-    );
+  fs.writeFile(destination, JSON.stringify(content, null, 4), (err) =>
+    err ? console.error(err) : console.info(`\nData written to ${destination}`)
+  );
 
+const readAndAppend = (content, file) => {
+  fs.readFile(file, 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+    } else {
+      const parsedData = JSON.parse(data);
+      parsedData.push(content);
+      writeToFile(file, parsedData);
+    }
+  });
+};
 
 // GET Route for homepage
 app.get('/', (req, res) =>
-    res.sendFile(path.join(__dirname, '/public/index.html'))
+  res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
 // GET Route for notes page
 app.get('/notes', (req, res) =>
-    res.sendFile(path.join(__dirname, '/public/notes.html'))
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
 // GET for notes db
 app.get('/api/notes', (req, res) => {
-    console.info(`${req.method} request received for notes`);
-    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+  console.info(`${req.method} request received for notes`);
+  readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
 });
 
 // POST for notes db
 app.post('/api/notes', (req, res) => {
+  // Log that a POST request was received
+  console.info(`${req.method} request received to submit feedback`);
 
-    writeToFile('./db/db.json', req.body)
+  // Destructuring assignment for the items in req.body
+  const { title, text } = req.body;
 
-    console.info(`${req.method} request received to add a note`);
+  // If all the required properties are present
+  if (title && text) {
+    let id = uuidv4()
+    // Variable for the object we will save
+    const newNote = {
+      title,
+      text,
+      id,
+    };
+    readAndAppend(newNote, './db/db.json');
+    const response = {
+      status: 'success',
+      body: newNote,
+    };
+    res.json(response);
+  } else {
+    res.json('Error in posting feedback');
+  }
 });
 
 
 
-
-
 app.listen(PORT, () =>
-    console.log(`App listening at http://localhost:${PORT} 🚀`)
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
 );
